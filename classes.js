a4093a422b7c570e8bfd9ddfe4f0d28f17c51496
sirchras <cjlynch@hotmail.co.nz>
2020-11-27 14:04:13 +1300

    Make fourth div visible

--- classes.js
+++ classes.js
@@ -10,6 +10,7 @@ function start () {
   // Your turn! Create a new function called `two`, then call it from here.
   two();
   three();
+  makeVisible();
 }
 
 function one () {
@@ -33,3 +34,7 @@ function three() {
 }
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var invisible=document.getElementsByClassName("invisible");
+  invisible[0].classList.add("visible");
+}
